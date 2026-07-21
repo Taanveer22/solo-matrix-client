b0817776a2d0f007d17074cf53127dac66603e03
Taanveer22 <taanveer469@gmail.com>
2026-07-21 07:16:34 +0600

    alhamdulliah bids data send to backend

---
+++ src/hooks/useAxiosCommon.jsx
@@ -0,0 +1,11 @@
+import axios from 'axios';
+
+const useAxiosCommon = () => {
+  const instance = axios.create({
+    baseURL: import.meta.env.VITE_API_URL,
+  });
+
+  return instance;
+};
+
+export default useAxiosCommon;
--- src/pages/JobCardDetails.jsx
+++ src/pages/JobCardDetails.jsx
@@ -2,17 +2,56 @@ import { useState } from 'react';
 import DatePicker from 'react-datepicker';
 import 'react-datepicker/dist/react-datepicker.css';
 import { useLoaderData } from 'react-router';
+import { toast } from 'react-toastify';
 import useAuth from '../hooks/useAuth';
+import useAxiosCommon from '../hooks/useAxiosCommon';
 
 const JobCardDetails = () => {
-  const [startDate, setStartDate] = useState(new Date());
+  const [selectedDate, setSelectedDate] = useState(new Date());
+  const axiosCommon = useAxiosCommon();
   const { user } = useAuth();
-  const loadedData = useLoaderData();
-  // console.log(loadedData);
-  const { deadline, category, jobTitle, description, minPrice, maxPrice, buyer } = loadedData;
+  const jobData = useLoaderData();
+  console.log(jobData);
+  const { _id, deadline, category, jobTitle, description, minPrice, maxPrice, client } = jobData;
 
-  const handleBidFormSubmit = (e) => {
+  const handleBidFormSubmit = async (e) => {
     e.preventDefault();
+    if (user?.email === client?.email) {
+      return toast.error('Action not permitted');
+    }
+
+    // pick bid form data
+    const bidPrice = parseFloat(e.target.bidPrice.value);
+    if (bidPrice < parseFloat(minPrice)) {
+      return toast.error('Bid at least min price');
+    }
+    const freelancerEmail = e.target.freelancerEmail.value;
+    const comment = e.target.comment.value;
+    const deadline = selectedDate;
+
+    const bidData = {
+      jobId: _id,
+      jobTitle,
+      category,
+      clientEmail: client?.email,
+      status: 'pending',
+      bidPrice,
+      freelancerEmail,
+      comment,
+      deadline,
+    };
+
+    console.log(bidData);
+
+    try {
+      const res = await axiosCommon.post(`/bids`, bidData);
+      console.log(res?.data);
+      if (res?.data?.insertedId) {
+        toast.success('Bid placed successfully');
+      }
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
@@ -27,19 +66,17 @@ const JobCardDetails = () => {
             {category}
           </span>
         </div>
-
         <div>
           <h1 className="mt-2 text-3xl font-semibold text-gray-800">{jobTitle}</h1>
-
           <p className="mt-2 text-lg text-gray-600">{description}</p>
-          <p className="mt-6 text-sm font-bold text-gray-600 ">Buyer Details:</p>
+          <p className="mt-6 text-sm font-bold text-gray-600 ">Client Details:</p>
           <div className="flex items-center gap-5">
             <div>
-              <p className="mt-2 text-sm  text-gray-600">Name: {buyer?.name}</p>
-              <p className="mt-2 text-sm  text-gray-600">Email: {buyer?.email}</p>
+              <p className="mt-2 text-sm  text-gray-600">Name: {client?.name}</p>
+              <p className="mt-2 text-sm  text-gray-600">Email: {client?.email}</p>
             </div>
             <div className="rounded-full object-cover overflow-hidden w-14 h-14">
-              <img src={buyer?.photo} alt="Photo" />
+              <img src={client?.photo} alt="Photo" />
             </div>
           </div>
           <p className="mt-6 text-lg font-bold text-gray-600 ">
@@ -51,33 +88,32 @@ const JobCardDetails = () => {
       {/* Place A Bid Form */}
       <section className="p-6 w-full rounded-md shadow-md flex-1 md:min-h-87.5">
         <h2 className="text-lg font-semibold text-gray-700 capitalize ">Place A Bid</h2>
-
         <form onSubmit={handleBidFormSubmit}>
           <div className="grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2">
             <div>
-              <label className="text-gray-700" htmlFor="price">
-                Price
+              <label className="text-gray-700" htmlFor="bidPrice">
+                Bid Price
               </label>
               <input
-                id="price"
-                type="text"
-                name="price"
+                id="bidPrice"
+                type="number"
+                name="bidPrice"
                 required
-                className="block w-full px-4 py-2 mt-2 text-gray-700 border-2 border-black rounded-md  focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40  focus:outline-none focus:ring"
+                className="block w-full px-4 py-2 mt-2 text-gray-700 border-2 border-black rounded-md focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40  focus:outline-none focus:ring"
               />
             </div>
 
             <div>
-              <label className="text-gray-700 " htmlFor="emailAddress">
-                Email Address
+              <label className="text-gray-700 " htmlFor="freelancerEmail">
+                Freelancer Email
               </label>
               <input
                 value={user?.email || ''}
-                id="emailAddress"
+                id="freelancerEmail"
                 type="email"
-                name="email"
+                name="freelancerEmail"
                 readOnly
-                className="block w-full px-4 py-2 mt-2 text-gray-700 border-2 border-black rounded-md focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40  focus:outline-none focus:ring"
+                className="block w-full px-4 py-2 mt-2 text-gray-700 border-2 border-black rounded-md focus:border-blue-400 focus:ring-blue-300 focus:ring-opacity-40 focus:outline-none focus:ring"
               />
             </div>
 
@@ -98,9 +134,9 @@ const JobCardDetails = () => {
 
               {/* Date Picker Input Field */}
               <DatePicker
-                className="border-2 border-black p-2 rounded-md"
-                selected={startDate}
-                onChange={(date) => setStartDate(date)}
+                className="border-2 border-black p-2 rounded-md w-full"
+                selected={selectedDate}
+                onChange={(date) => setSelectedDate(date)}
                 minDate={new Date()}
               />
             </div>
